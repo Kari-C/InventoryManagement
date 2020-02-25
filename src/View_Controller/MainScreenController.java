7fb1e52955f9d566bb03f6b403b2e1ea4afe5fab
JenJetson <kari@jenjetson.com>
2020-02-25 06:06:51 -0800

    All regular java classes and GUI fx:id's done.  Need to add interactive functions next.

--- src/View_Controller/MainScreenController.java
+++ src/View_Controller/MainScreenController.java
@@ -1,4 +1,21 @@
 package View_Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class MainScreenController {
+
+    public MainScreenController() throws IOException {
+    }
+
+     public void btnPartAddClick(javafx.event.ActionEvent actionEvent) throws IOException {
+    }
 }
